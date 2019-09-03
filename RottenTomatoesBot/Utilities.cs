247185dd6469b29505ccbff63f05dd20c71d2da9
William <WilliamWelsh@users.noreply.github.com>
2019-09-03 15:37:27 -0400

    Removed unused namespaces

--- RottenTomatoesBot/Utilities.cs
+++ RottenTomatoesBot/Utilities.cs
@@ -4,8 +4,6 @@ using System.Text;
 using Discord.WebSocket;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using System.Threading;
-using System.IO;
 
 namespace RottenTomatoes
 {

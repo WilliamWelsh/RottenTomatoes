f3c51fa20362fcecb474d46cde01fee2ada06427
William <WilliamWelsh@users.noreply.github.com>
2021-02-12 14:29:38 -0500

    Updated my (developer) discriminator

--- RottenTomatoesBot/Commands.cs
+++ RottenTomatoesBot/Commands.cs
@@ -40,10 +40,10 @@ namespace RottenTomatoes
         // Prints a link to my Discord
         // This is so a user can report something to me, or ask for a new feature
         [Command("discord")]
-        public async Task RTDiscord() => await Context.Channel.SendEmbed("Bot Help", "Hello, if you need help with the bot, or need to report a bug, or request a new feature, please join my server and contact me\n(`Reverse#1193`)\nhttps://discord.gg/ga9V5pa", true);
+        public async Task RTDiscord() => await Context.Channel.SendEmbed("Bot Help", "Hello, if you need help with the bot, or need to report a bug, or request a new feature, please join my server and contact me\n(`Reverse#0069`)\nhttps://discord.gg/ga9V5pa", true);
 
         // Posts privacy information
         [Command("privacy")]
-        public async Task PrivacyInfo() => await Context.Channel.SendEmbed("Privacy", $"The only data this bot stores is the server ID (`{Context.Guild.Id}`) of this server. This is temporary, and all data is deleted everytime the bot restarts or goes offline. Your server ID is used to create a server handler to handle your search request. This way, multiple searches may be done at the same time in separate servers. The bot requires this system to work effectively. If you wish to not participate, please remove the bot from your server. Your server ID is not used for anything else.\n\nIf you wish to speak to the developer (`Reverse#1193`) please type `!rt discord` and join the support server, or just add me.", true);
+        public async Task PrivacyInfo() => await Context.Channel.SendEmbed("Privacy", $"The only data this bot stores is the server ID (`{Context.Guild.Id}`) of this server. This is temporary, and all data is deleted everytime the bot restarts or goes offline. Your server ID is used to create a server handler to handle your search request. This way, multiple searches may be done at the same time in separate servers. The bot requires this system to work effectively. If you wish to not participate, please remove the bot from your server. Your server ID is not used for anything else.\n\nIf you wish to speak to the developer (`Reverse#0069`) please type `!rt discord` and join the support server, or just add me.", true);
     }
 }
